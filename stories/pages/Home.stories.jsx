8fe5a917e175dd3d0b081f7515a1926fefef58c1
bsisic <bsisic.42@gmail.com>
2021-11-26 09:00:23 +0100

    [init] next js create app and storybook added

---
+++ stories/pages/Home.stories.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Home from "../../pages/index";
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
+
+export const HomePage = () => <Home />
