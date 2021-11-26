7fdcf7ab106e6b8a79ce73b444921a9798ceff41
bsisic <bsisic.42@gmail.com>
2021-11-26 11:05:23 +0100

    [web3] connect wallet

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import React, {useEffect, useState} from 'react'
 import Head from 'next/head'
+
 import styles from '../styles/Home.module.css'
 import Footer from '../components/Footer'
 
+let Web3 = require('web3')
+
 export default function Home() {
+  const [web3, setWeb3] = useState(null)
+  const [address, setAddress] = useState(null)
+  const [contract, setContract] = useState(null)
+
+  useEffect(() => {
+    window.ethereum ?
+      ethereum.request({ method: "eth_requestAccounts" }).then((accounts) => {
+        setAddress(accounts[0])
+        let w3 = new Web3(ethereum)
+        setWeb3(w3)
+      }).catch((err) => console.log(err))
+    : console.log("Please install MetaMask")
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,8 +31,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="">Nfight!</a>
+          Welcome to <a href="">NFighT!</a>
         </h1>
+        { address ? <p>Address : {address}</p> : null }
       </main>
 
       <Footer/>
