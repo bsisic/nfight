067565326eac57a1ad3b81f8a3d24a165424453b
bsisic <bsisic.42@gmail.com>
2021-12-20 13:55:00 +0100

    [404] custom error page

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+export default function Custom404() {
+    return (
+        <div style={{
+            width: '100vw',
+            height: '100vh',
+            backgroundColor: '#000000',
+            color: '#fff',
+            overflowX: 'hidden',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            position: 'absolute',
+            zIndex: '10'
+        }}>
+            <h1>404 - Page not found</h1>
+            <h3>NFighT @ Elliot Corp</h3>
+        </div>
+    )
+  }
